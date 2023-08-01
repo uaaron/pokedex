aed50bef56756c93536fd7bea061be017e381730
Uaaron Sihan <uaaronsihan@gmail.com>
2023-08-01 14:43:54 -0700

    added list dataset for pokemon
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello World');
-let favoriteFood = "Tiramisu"
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [{name: "Bulbasaur", height: .7, type: ["grass", "poison"]}, 
+                {name: "Charmander", height: .6, type: "fire"},
+                {name: "Squirtle", height: .5, type: "water"}];
