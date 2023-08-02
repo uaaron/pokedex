24803bc51ef1b6446e0d0f379afd5ee124489654
Uaaron Sihan <uaaronsihan@gmail.com>
2023-08-02 13:20:04 -0700

    looped through dataset to display list
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-let pokemonList = [];
-pokemonList = [{name: "Bulbasaur", height: .7, type: ["grass", "poison"]}, 
-                {name: "Charmander", height: .6, type: "fire"},
-                {name: "Squirtle", height: .5, type: "water"}];
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: .7,
+        type: ["grass", "poison"]
+    }, 
+    {
+        name: "Charmander",
+        height: .6,
+        type: "fire"
+    },
+    {
+        name: "Squirtle",
+        height: .5,
+        type: "water"
+    }
+];
+    
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0.6) {
+        document.write(`<p>${pokemonList[i].name} height: (${pokemonList[i].height}) - Wow, that's big!</p>`);
+    } else {
+        document.write(`<p>${pokemonList[i].name} height: (${pokemonList[i].height})</p>`);
+    }
+}
